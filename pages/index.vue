09d561c9bc1af2b93860ca3b7e3704a02958800a
Edwin Chang <edwinchang2024@gmail.com>
2022-06-19 19:09:33 -0700

    🎨 Fix formatting

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-    const color = ref("#000000")
+    const color = ref("#000000");
     function randomColor() {
         var newColor = "";
         for (var i = 0; i < 6; i++) {
             newColor += Math.floor(Math.random() * 16).toString(16);
         }
-        color.value = "#" + newColor
+        color.value = "#" + newColor;
     }
 </script>
 
 <template>
     <div class="grid place-items-center h-screen">
-        <p @click="randomColor" class="font-sans text-3xl select-none" :style='{"color":color}'>
+        <p @click="randomColor" class="font-sans text-3xl select-none" :style="{ color: color }">
             Nothing here yet! Check back later.
         </p>
     </div>
