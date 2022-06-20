c98960b196404c33d3b8d9fa12df8c1192050b7c
Edwin Chang <edwinchang2024@gmail.com>
2022-06-19 19:08:48 -0700

    💄 Add notice

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
-<script setup lang="ts">
-    const count = ref(0);
-    function inc() {
-        count.value++;
-    }
-</script>
-
 <template>
-    <div class="font-sans">
-        <h1 class="text-xl font-bold">Hi</h1>
-        <p>yay</p>
-        <button @click="inc">Click me!</button>
-        <p>{{ count }}</p>
+    <div>
+        <NuxtPage />
     </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    const color = ref("#000000")
+    function randomColor() {
+        var newColor = "";
+        for (var i = 0; i < 6; i++) {
+            newColor += Math.floor(Math.random() * 16).toString(16);
+        }
+        color.value = "#" + newColor
+    }
+</script>
+
+<template>
+    <div class="grid place-items-center h-screen">
+        <p @click="randomColor" class="font-sans text-3xl select-none" :style='{"color":color}'>
+            Nothing here yet! Check back later.
+        </p>
+    </div>
+</template>
