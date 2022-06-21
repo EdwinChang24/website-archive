9da8aeb5947855ada0b64885a32cf050da2003ec
Edwin Chang <edwinchang2024@gmail.com>
2022-06-20 22:50:00 -0700

    🏗️ Switch to Tailwind official package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: ["~/assets/css/tailwind.css"],
-    modules: ["@nuxtjs/tailwindcss"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
     meta: {
         title: "Edwin Chang",
         charset: "UTF-8",
