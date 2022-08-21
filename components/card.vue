bb53a5c0bfb9314e13c81c2e6df3cfcc223fd618
Edwin Chang <edwinchang2024@gmail.com>
2022-08-21 15:48:57 -0700

    ✨ Add card

---
+++ components/card.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="bg-white rounded border border-slate-700 p-8 transition-all">
+        <h1 class="text-3xl font-semibold mb-4">Hi, I'm Edwin</h1>
+        <p>
+            I'm a student interested all sorts of modern software. My main languages are Python, Java, Kotlin, and
+            TypeScript. Check out my
+            <a class="text-blue-600 hover:underline" href="https://github.com/EdwinChang24/">GitHub</a>!
+        </p>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,16 @@
 <script setup lang="ts">
     import isMobile from "is-mobile";
-
-    const color = ref("#000000");
-    function randomColor() {
-        var newColor = "";
-        for (var i = 0; i < 6; i++) {
-            newColor += Math.floor(Math.random() * 16).toString(16);
-        }
-        color.value = "#" + newColor;
-    }
 </script>
 
 <template>
     <div>
-        <div
-            class="absolute w-screen h-screen bg-bottom bg-[url('/grid.svg')]"
-            style="mask-image: radial-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 1))"
-        />
-        <Background v-if="!isMobile()" class="absolute h-screen w-screen" />
-        <div class="absolute inset-1/3 flex justify-center items-center">
-            <p
-                @click="randomColor"
-                class="self-center text-center font-sans text-2xl select-none"
-                :style="{ color: color }"
-            >
-                Not much here yet. Maybe check back later.
-            </p>
+        <div class="absolute w-screen h-screen bg-slate-100">
+            <div
+                class="absolute w-screen h-screen bg-bottom bg-[url('/grid.svg')]"
+                style="mask-image: radial-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 1))"
+            />
+            <Background v-if="!isMobile()" class="absolute h-screen w-screen" />
+            <Card class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
         </div>
     </div>
 </template>
