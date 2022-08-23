d20a9138733fd222025e80b2cd81b72cc79b5fb4
Edwin Chang <edwinchang2024@gmail.com>
2022-08-22 17:41:57 -0700

    💄 Add GitHub button

--- components/card.vue
+++ components/card.vue
@@ -1,12 +1,15 @@
-<script setup lang="ts"></script>
-
 <template>
     <div class="bg-white rounded border border-slate-700 p-8 transition-all">
         <h1 class="text-3xl font-semibold mb-4">Hi, I'm Edwin</h1>
-        <p>
+        <p class="mb-4">
             I'm a student interested all sorts of modern software. My main languages are Python, Java, Kotlin, and
             TypeScript. Check out my
             <a class="text-blue-600 hover:underline" href="https://github.com/EdwinChang24/">GitHub</a>!
         </p>
+        <div class="h-10 flex flex-row-reverse">
+            <a target="_blank" href="https://github.com/EdwinChang24/">
+                <img class="float-right w-8 h-auto transition-all hover:w-10" src="/github.svg" />
+            </a>
+        </div>
     </div>
 </template>
